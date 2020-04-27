59e555a05bdea9f3c3be81874517e2e2a8a6bd1a
fayed2 <fayeed094@gmail.com>
2020-04-28 01:24:01 +0200

    ADC with 2 sensor to save the owe of CPU and connect it to interrupt 0

--- Amain.c
+++ Amain.c
@@ -23,12 +23,22 @@ char message[] ="hello bro!";
 char st1[]="volt = ";
 char st2[]= "mv";
 char cl[]="    ";
+
+void INT0_init() 
+{
+    MCUCR |= (1 << ISC01) | (1 << ISC00); // Rising Edge
+    GICR |= (1 << INT0); 
+}
+
 void ADC_init() 
 {
     ADMUX = 0x00; // Channel 0
-    //SFIOR |=  (1<<ADTS1);
-    ADCSRA |= (1 << ADEN) |(1 << ADIE) | (1 << ADPS2) |(1 << ADPS1) | (1 << ADPS0) ;
+    SFIOR |=  (1<<ADTS1); //special function input output register 
+    // SFIOR must be before ADCSRA !!!!!! 
+    ADCSRA |= (1 << ADEN) | (1<<ADATE) |(1 << ADIE) | (1 << ADPS2) |(1 << ADPS1) | (1 << ADPS0) ;
      // if i needed to put the pre scaler with f/128
+    //AADATE is auto trigger enable to connect between ADC and interrupt0
+    //SFIOR |=  (1<<ADTS1); //special function input output register
 }
 void startConv() 
 {
@@ -37,6 +47,7 @@ void startConv()
 
 void Selecting_channel(unsigned int channel )
 {
+    
     ADMUX &= ~(7<<0);  //00000111 11111000
     ADMUX |= channel;  
 }
@@ -57,7 +68,6 @@ int getADCdataL()
     return data;
 }
 
-
 ISR(ADC_vect)
 {
     static int state =0 ;
@@ -72,10 +82,10 @@ ISR(ADC_vect)
     
     if (state)
     {
-        LCD_String_xy(1,7,cl);
-        LCD_String_xy(1,7,buffer);
-        startConv();
-        state=0;
+        LCD_String_xy(1,7,cl); // clearing lcd to overwrite
+        LCD_String_xy(1,7,buffer);  // data after conversion from ADC
+        startConv(); // infinite loop to start conversion right now !!
+        state=0;      // changing state to read from the other sensor
     }
     else 
     {
@@ -84,20 +94,29 @@ ISR(ADC_vect)
         startConv();
         state=1;
     }
-    _delay_ms(500);
+    _delay_ms(500);  // delay to adjust the ADC O/P
 }
 
+ISR(INT0_vect)
+{
+    static int triggered=1;
+    if(triggered)
+    {
+        startConv();
+        triggered=0; // set it to zero to confirm that I entered it once
+    }
+}
 
 int main(void) {
     /* Replace with your application code */
     PORTCas(OUT);
     PORTDas(OUT);
         
-    LCD_Init();
-    ADC_init(); // input to trigger is  on ADC0
-    
+    LCD_Init(); //initializing the LCD
+    ADC_init(); // input to trigger is  on ADC0 and ADC 1
+    INT0_init();//  setting interrupt int0  ON as well as adjusting interrupt on BUTTON2 
     sei();   //Global interrupt is ON
-    startConv(); 
+   // startConv();  it was set when Interrupt is triggered
     
     LCD_String_xy(0, 0, st1);
     LCD_String_xy(0, 13, st2);
