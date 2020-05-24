70153b67cc77ee78206d74d348403f45d1f8e23a
AHMED MOHAMED IBRAHIM ABDELRAHMAN FAYED <62766231+afayed95@users.noreply.github.com>
2020-05-24 05:03:53 +0200

    Timer drivers with PWM ,FPWM, NORMAL,OC0

--- main.c
+++ main.c
@@ -2,31 +2,70 @@
  * File:   main.c
  * Author: Safaa
  *
- * Created on May 1, 2020, 9:31 AM
+ * Created on May 1, 2020, 11:27 AM
  */
 
 #define F_CPU 8000000UL
-#include <xc.h>
+#include <avr/io.h>
+#include <util/delay.h>
 #include <avr/interrupt.h>
-#include "mADC.h"
+#include "mTimer.h"
 
+#define LED     0
+#define Btn0    1
+#define Btn1    2
 
-int data;
+//30 times per second
+
+ISR(TIMER0_OVF_vect) {
+    static int _1sec = 0;
+    _1sec++;
+    if (_1sec == 31) {
+        // Once every on second
+        PORTC ^= (1 << LED); // LED Toggle
+        _1sec = 0;
+    }
+}
+int isPressed(int pinNum);
+
+int dCycle = 50;
 
 int main(void) {
+    /* Replace with your application code */
 
-    ADC_init( 0 , _AREF, _128PS, _useInterrupt, _noAutoTrigger);
-    sei();
+    DDRC |= (1 << LED);
+    DDRC &= ~((1 << Btn0) | (1 << Btn1));
+    PORTC &= ~(1 << LED);
 
-    ADC_startConv();
-    DDRC = 0xFF;
-    DDRD = 0xFF;
+    //  Timer0_init(char TimerMode, char CompOUTMode, char Clk_S)
+    Timer0_setDutyCyle(dCycle);
+    Timer0_init(PWM, OC0_PWM_ClearDN, CLK_io_PS1024);
+
+    sei(); // Global Interrupt Enable
     while (1) {
+        if (isPressed(Btn0)) {
+            if (dCycle > 0) {
+                dCycle -= 10;
+                Timer0_setDutyCyle(dCycle);
+            }
 
-        PORTC = (char) data;
-        PORTD = (char) (data >> 8);
+        }
 
+        if (isPressed(Btn1)) {
+            if (dCycle < 100)
+                dCycle += 10;
+            Timer0_setDutyCyle(dCycle);
+        }
+
+        _delay_ms(500);
     }
-    return 0;
 }
 
+int isPressed(int pinNum) {
+
+    if (PINC & (1 << pinNum)) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
